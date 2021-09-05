e8758816a7e610cee42a4b36394218042abd456e
Juliano Canuto <julianocanuto@gmail.com>
2021-09-04 23:53:31 -0300

    Project setup completed

---
+++ src/main/java/br/com/julianocanuto/managebooks/ManageBooksApplication.java
@@ -0,0 +1,13 @@
+package br.com.julianocanuto.managebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/julianocanuto/managebooks/ManageBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julianocanuto.managebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
